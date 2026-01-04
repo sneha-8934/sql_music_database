f3f324f0f42e19b458b474d15a060ff362004455
V Sneha Naga Harshitha <harshithav257@gmail.com>
2026-01-04 17:54:49 +0530

    Update SQL_music_store_analysis_project.sql

--- SQL_music_store_analysis_project.sql
+++ SQL_music_store_analysis_project.sql
@@ -1,3 +1,11 @@
+Questions
+
+1. Who is the senior most employee based on job title?
+2. Which countries have the most Invoices?
+3. What are top 3 values of total invoice?
+4. Which city has the best customers? We would like to throw a promotional Music Festival in the city we made the most money. Write a query that returns one city that has the highest sum of invoice totals. Return both the city name & sum of all invoice totals
+5. Who is the best customer? The customer who has spent the most money will be declared the best customer. Write a query that returns the person who has spent the most money  
+
 Q1
 SELECT * from employee
 ORDER by levels DESC limit 1
@@ -87,6 +95,7 @@ order by customer.first_name, customer.last_name,country,sum(total)  DESC
 select * from customer_spent_countrywise
 where row_num<=1
 order by customer_spent_countrywise.country
+
 
 
 
