fcd59e2200b860ce9d5c3f13438a49891a2f9ed1
V Sneha Naga Harshitha <harshithav257@gmail.com>
2026-01-04 17:55:42 +0530

    Update SQL_music_store_analysis_project.sql

--- SQL_music_store_analysis_project.sql
+++ SQL_music_store_analysis_project.sql
@@ -5,7 +5,11 @@ Questions
 3. What are top 3 values of total invoice?
 4. Which city has the best customers? We would like to throw a promotional Music Festival in the city we made the most money. Write a query that returns one city that has the highest sum of invoice totals. Return both the city name & sum of all invoice totals
 5. Who is the best customer? The customer who has spent the most money will be declared the best customer. Write a query that returns the person who has spent the most money  
+6. Write query to return the email, first name, last name, & Genre of all Rock Music listeners. Return your list ordered alphabetically by email starting with A
+7. Let's invite the artists who have written the most rock music in our dataset. Write a query that returns the Artist name and total track count of the top 10 rock bands
+8. Return all the track names that have a song length longer than the average song length. Return the Name and Milliseconds for each track. Order by the song length with the longest songs listed first
 
+  
 Q1
 SELECT * from employee
 ORDER by levels DESC limit 1
@@ -95,6 +99,7 @@ order by customer.first_name, customer.last_name,country,sum(total)  DESC
 select * from customer_spent_countrywise
 where row_num<=1
 order by customer_spent_countrywise.country
+
 
 
 
