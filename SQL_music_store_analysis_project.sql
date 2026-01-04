85f550f7e617da90ef30bbe8dacf4a255090f7c5
V Sneha Naga Harshitha <harshithav257@gmail.com>
2026-01-04 17:57:24 +0530

    Update SQL_music_store_analysis_project.sql

--- SQL_music_store_analysis_project.sql
+++ SQL_music_store_analysis_project.sql
@@ -8,28 +8,37 @@ Questions
 6. Write query to return the email, first name, last name, & Genre of all Rock Music listeners. Return your list ordered alphabetically by email starting with A
 7. Let's invite the artists who have written the most rock music in our dataset. Write a query that returns the Artist name and total track count of the top 10 rock bands
 8. Return all the track names that have a song length longer than the average song length. Return the Name and Milliseconds for each track. Order by the song length with the longest songs listed first
+9. Find how much amount spent by each customer on artists? Write a query to return customer name, artist name and total spent
+10. We want to find out the most popular music Genre for each country. We determine the most popular genre as the genre with the highest amount of purchases. Write a query that returns each country along with the top Genre. For countries where the maximum number of purchases is shared return all Genres
+11. Write a query that determines the customer that has spent the most on music for each country. Write a query that returns the country along with the top customer and how much they spent. For countries where the top amount spent is shared, provide all customers who spent this amount
+  
 
   
 Q1
 SELECT * from employee
 ORDER by levels DESC limit 1
+  
 Q2
 SELECT billing_country,count(*) as tot_invoices  from invoice 
 GROUP by billing_country order by tot_invoices desc;
+  
 Q3
 select * from invoice
 SELECT invoice.total
 from invoice
-ORDER by total desc limit 3
+ORDER by total desc limit 3;
+  
 Q4
 select * from invoice
 SELECT billing_city, sum(total) from invoice
-GROUP by billing_city ORDER by sum(total) desc
+GROUP by billing_city ORDER by sum(total) desc;
+  
 Q5
 SELECT customer.customer_id,customer.first_name,customer.last_name, sum(invoice.total) from customer
 join invoice on customer.customer_id=invoice.customer_id
 group by customer.customer_id
-ORDER by sum(invoice.total) desc limit 1
+ORDER by sum(invoice.total) desc limit 1;
+  
 Q6
 SELECT distinct email, first_name,customer.last_name, genre.name
 from customer
@@ -38,7 +47,8 @@ join invoice_line on invoice.invoice_id = invoice_line.invoice_id
 join track on invoice_line.track_id = track.track_id
 join genre on track.genre_id=genre.genre_id
 where genre.name like 'Rock'
-order by email
+order by email;
+  
 Q7
 select artist.artist_id, artist.name, count(artist.artist_id) as tot_songs
 from artist
@@ -48,11 +58,13 @@ join genre on genre.genre_id = track.genre_id
 where genre.name like 'Rock'
 group by artist.artist_id
 order by tot_songs DESC
-limit 10
+limit 10;
+  
 Q8
 SELECT track.name, milliseconds as  song_length
 from track where milliseconds >(SELECT avg(milliseconds) as avg_song_length from track)
-order by milliseconds desc
+order by milliseconds desc;
+  
 Q9
 with best_selling_artist AS(
 SELECT artist.artist_id,artist.name,sum(invoice_line.unit_price* invoice_line.quantity) as tot_price
@@ -73,6 +85,7 @@ join album on album.album_id = track.album_id
 join best_selling_artist bsa on bsa.artist_id=album.artist_id
 group by 1,2,4
 order by 5 desc;
+  
 Q10
 with popular_genre as(
 SELECT customer.country,genre.genre_id,genre.name, count(invoice_line.quantity) as purchases,
@@ -86,7 +99,8 @@ group by customer.country,genre.genre_id,genre.name
 order by customer.country asc,count(invoice_line.quantity) desc 
 )
 SELECT * from popular_genre
-where row_num <=1
+where row_num <=1;
+  
 Q11
 with customer_spent_countrywise as(
 select customer.customer_id, customer.first_name, customer.last_name, customer.country,sum(total),
@@ -99,6 +113,7 @@ order by customer.first_name, customer.last_name,country,sum(total)  DESC
 select * from customer_spent_countrywise
 where row_num<=1
 order by customer_spent_countrywise.country
+
 
 
 
